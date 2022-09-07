3613c2511eedc6e8cb2d435c57665f83ba1152d5
ceb <vivu1991@mail.ru>
2022-09-07 20:58:53 +0300

    DZ 04 решение четвертой задачи

---
+++ DZ004/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 19 HARD - необязательная
+//Напишите программу, которая принимает на вход число любой размерности и проверяет, является ли оно палиндромом.
+
+bool Palindrom(int x)
+{
+    int i = 0;
+    int j = x;
+    while (j != 0) 
+    {
+        i = (i * 10) + (j % 10);
+        j = j / 10;
+    }
+    return (i == x);
+}
+
+void PrintResult(string [] args)
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (Palindrom(x) == true) Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Число не является палиндромом");
+    
+}
+PrintResult(args);
