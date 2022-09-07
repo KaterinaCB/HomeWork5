0ab84e1c9c14462f224ffaca7d2876b0831ecbba
ceb <vivu1991@mail.ru>
2022-09-07 18:39:23 +0300

    DZ 02 решение второй задачи

---
+++ DZ002/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int [] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-50, 50);
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}");
+    }
+    return array;
+}
+int [] mas = FillArray(10);
+
+int FindSum(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum += array[i];
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях равна -> {FindSum(mas)}");
